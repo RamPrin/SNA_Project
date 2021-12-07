12583070a9851a0c5671554b7681939641ca8e08
tsepanx <77626319+tsepanx@users.noreply.github.com>
2021-12-07 14:02:57 +0300

    Api: add ability to filter objects by query params

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,26 @@
 from rest_framework import viewsets
 
-from blog.models import Article
-from blog.serializers import ArticleSerializer
+from .models import Article
+from .serializers import ArticleSerializer
 
 
 class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+
+    def get_queryset(self):
+        queryset = Article.objects.all()
+
+        query_params_filter_mapping = {
+            'username': 'author__username',
+            'user_id': 'author_id',
+        }
+
+        for query_param, filter_by in query_params_filter_mapping.items():
+            param_value = self.request.query_params.get(query_param)
+
+            if param_value is not None:
+                kwargs = {filter_by: param_value}
+                queryset = queryset.filter(**kwargs)
+
+        return queryset
