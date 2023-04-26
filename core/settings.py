2c1a1bf51b794ea566771235dbf161b018c7a27e
tsepanx <77626319+tsepanx@users.noreply.github.com>
2023-04-26 17:13:13 +0300

    postgresql instead of sqlite

--- core/settings.py
+++ core/settings.py
@@ -102,9 +102,20 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sna_blog',
+        'USER': 'postgres',
+        # 'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        "OPTIONS": {
+            "server_side_binding": True,
+        }
     }
 }
 
